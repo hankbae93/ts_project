371c583dce060aea48462e295ab90b9bd0b9c47e
ranja <seolin9340@gmail.com>
2022-04-16 14:30:30 +0900

    [Fix] Iframe 상단 텍스트로 인한 css 깨짐 오류 수정

--- src/components/Viewer/style.ts
+++ src/components/Viewer/style.ts
@@ -4,15 +4,19 @@ export const Container = styled.div`
 	flex: 1;
 	display: flex;
 	flex-direction: column;
+	width: 100%;
 	height: 100%;
 `;
 
 export const IframeHead = styled.div`
-	word-wrap: nowrap;
-
 	position: relative;
+	width: calc(100vw - 280px);
 	height: 50px;
-	padding: 17px;
+	padding: 17px calc(17px * 3) 17px 17px;
+	overflow: hidden;
+	white-space: nowrap;
+	-webkit-box-orient: vertical;
+	text-overflow: ellipsis;
 	background-color: #fff;
 	box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
 	box-sizing: border-box;
